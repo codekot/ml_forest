9b05134a323a4c71c4aa1d1a9d0c9d4692cbb61c
codekot <gennadii.shamshurin@gmail.com>
2022-04-28 18:07:43 +0400

    testing basic scripting

--- src/ml_forest/eda.py
+++ src/ml_forest/eda.py
@@ -1,2 +1,8 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+
 def eda():
-    print("Testing profiling")
+    df = pd.DataFrame([[1,2,3], ["red", "green", "blue"], [55,11,77],[1000,1200,1500]])
+    profile = ProfileReport(df)
+    profile.to_file("output.html")
+
