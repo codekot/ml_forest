25fd3aeddbf10f2acfe96bf8c260c70ae3b14292
codekot <gennadii.shamshurin@gmail.com>
2022-05-04 17:55:21 +0400

    change EDA to actual data with hardcode path

--- src/ml_forest/eda.py
+++ src/ml_forest/eda.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pandas_profiling import ProfileReport
 
 def eda():
-    df = pd.DataFrame([[1,2,3], ["red", "green", "blue"], [55,11,77],[1000,1200,1500]])
+    df = pd.read_csv(r"D:\CODE\RSML\ml_forest\data\train.csv")
     profile = ProfileReport(df)
     profile.to_file("output.html")
 
