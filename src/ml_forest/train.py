ab4f232390fa0346d52e55e34e3ed7cb66d147ee
codekot <gennadii.shamshurin@gmail.com>
2022-05-05 17:38:03 +0400

    add LogisticRegression model and saving model

--- src/ml_forest/train.py
+++ src/ml_forest/train.py
@@ -1,8 +1,15 @@
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import joblib
 
 def train():
     target_column = "Cover_Type"
     dataset = pd.read_csv(r"D:\CODE\RSML\ml_forest\data\train.csv")
-    features = dataset.drop(target_column, axis=1)
-    target = dataset[target_column]
+    X = dataset.drop(target_column, axis=1)
+    y = dataset[target_column]
+    model = LogisticRegression(solver='saga', random_state=42).fit(X, y)
+    filename = '.\data\model.sav'
+    joblib.dump(model, filename)
+    print("Train script finished")
+
 
